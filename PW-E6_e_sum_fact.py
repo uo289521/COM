7001f3bc2c77406b39862667a550b25dc1c7fd4f
UO289521 <uo289521@uniovi.es>
2023-11-22 16:52:02 +0100

    final clase 22/11

--- PW-E6_e_sum_fact.py
+++ PW-E6_e_sum_fact.py
@@ -7,20 +7,49 @@ def pred(Z):
         return 0
 
 		
-""" PW-E6-e): Construir un PW que compute f(X)=sum_i=0..X(i!). Empleando macros: producto y asignacion """ 
-# Pasar a f como argumento las k varibles (X1, X2, ...Xk)  del programa while k variables construido
-def pw(...):
-
-
+"""
+x1 < la entrada
+x2 < iterar de 1
+x3 < calcular el factorial
+x4 < calculamos el sumatorio        
+            caso especial 
+                sum 1=0 (0!) = 1
+"""
+def pw(X1,X2,X3,X4):
+    X3 = 1  # caso especial
+    X4 = 1  # sum X=0..0 (0!) = 1
+    while X2 != X1:
+        X2 = succ(X2)
+        X3 = X3 * X2
+        X4 = X4 + X3
+    X1 = X4
     return X1
 
 def sum_fact(X):
-  return print(pw(X,...))
+  return print(pw(X,0,0,0))
 
 
 # Para probar el programa, invocar a sum_fact	
 # Probar que sucede para varios valores de X: 0, 1, 2, ...
-sum_fact(...)	
+sum_fact(5)
 
 
 
+"""
+begin 
+    X5 = X1
+    Pfact
+    X6 = X1
+    while X7 != X5 do
+    begin  
+        X7: succ(X7)
+        #Preparo vector estado para el fact 
+        X1 = X7
+        X2 = 0
+        X3 = 0
+        Pfact;
+        X6 = X6 + X1
+    end
+    X1=X6 
+    return X1;     
+"""
